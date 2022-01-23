74fa5d32c9d4a360dd54c30eb4092ce4f6cf62e3
apollorion <joey@apollorion.com>
2022-01-23 15:55:58 -0500

    error_cachine_min_ttl

--- terraform/cdn.tf
+++ terraform/cdn.tf
@@ -16,9 +16,10 @@ module "cdn" {
   website_enabled     = true
 
   custom_error_response = [{
-    error_code         = 404
-    response_code      = 200
-    response_page_path = "index.html"
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "index.html"
+    error_caching_min_ttl = 86400
   }]
 }
 
