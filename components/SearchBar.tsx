b66ff90ba54311af6c56dba9eac1a196eea5cfcf
apollorion <joey@apollorion.com>
2023-02-21 16:22:00 -0500

    feat(components): fix search bar for chrome users

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,20 +2,25 @@ import {availableItemVersions} from "@/lib/oaspec";
 
 function SearchBar({pageItem, pageVersion}: {pageItem: string, pageVersion: string}){
     const itemVersions = availableItemVersions();
+
+    const handleChanges = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        window.location.href=event.target.value
+    }
+
     return (
         <div>
             <select
                 style={{width: "50ch", textAlignLast: "center"}}
                 name="product"
                 id="product"
-                defaultValue={`${pageItem}/${pageVersion}`}
+                defaultValue={`/${pageItem}/${pageVersion}`}
+                onChange={handleChanges}
             >
                 {Object.keys(itemVersions).map((item) => (
                     <optgroup key={item} label={item}>
                         {itemVersions[item].map((version) => (
-                            <option key={`${item}/${version}`}
-                                    value={`${item}/${version}`}
-                                    onClick={() => window.location.href=`/${item}/${version}`}
+                            <option key={`/${item}/${version}`}
+                                    value={`/${item}/${version}`}
                             >{`${item} - ${version}`}</option>
                         ))}
                     </optgroup>
