f3e6e385765c80390c2421d22404085189a39a6b
Bumhan Yu <bumhan.yu@gmail.com>
2023-02-22 22:26:27 -0500

    update contribution notes in the footer

--- components/HeartApollorion.tsx
+++ components/HeartApollorion.tsx
@@ -1,5 +1,16 @@
+import styles from "./HeartApollorion.module.css";
+
 export default function HeartApollorion() {
     return (
-        <p style={{marginTop: "50px", marginBottom: "50px"}}>K8s Is Awesome. Made with ❤, <a href="https://github.com/apollorion/manifests.io">apollorion</a>.</p>
+        <div className={styles.note}>
+            <p>
+                <span><a href="https://github.com/apollorion/manifests.io">View in GitHub</a></span> • <span>K8s Is Awesome</span> • <span>Made with <span>❤</span></span>
+            </p>
+            <p style={{fontSize: "smaller", marginTop: "5px"}}>
+                
+                Authored by <a href="https://github.com/apollorion/">apollorion</a> •{' '}
+                Design support by <a href="https://github.com/baadaa/">baadaa</a>
+            </p>
+        </div>
     )
 }
