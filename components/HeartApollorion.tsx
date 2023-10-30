ae51dab79029650c760c7825255586da9bc2647c
apollorion <joey@apollorion.com>
2023-10-30 19:46:47 -0400

    style(components): let intellij reformat all the code

--- components/HeartApollorion.tsx
+++ components/HeartApollorion.tsx
@@ -7,7 +7,7 @@ export default function HeartApollorion() {
                 <span><a href="https://github.com/apollorion/manifests.io">View in GitHub</a></span> • <span>K8s Is Awesome</span> • <span>Made with <span>❤</span></span>
             </p>
             <p style={{fontSize: "smaller", marginTop: "5px"}}>
-                
+
                 Authored by <a href="https://github.com/apollorion/">apollorion</a> •{' '}
                 Design support by <a href="https://github.com/baadaa/">baadaa</a>
             </p>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 import ErrorBoundary from "@/components/ErrorBoundary";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-      <ErrorBoundary>
-        <Component {...pageProps} />
-      </ErrorBoundary>
-  );
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <ErrorBoundary>
+            <Component {...pageProps} />
+        </ErrorBoundary>
+    );
 }
