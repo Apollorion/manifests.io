c50933fc81bc74c2d9a62982aa5eef80c952a933
Chris Funderburg <chris.funderburg@element.com>
2024-03-14 00:14:23 +0000

    adding latest certmanager

--- lib/oaspec.tsx
+++ lib/oaspec.tsx
@@ -7,6 +7,7 @@ import kubernetes125 from "../oaspec/kubernetes/1.25.json";
 import kubernetes124 from "../oaspec/kubernetes/1.24.json";
 
 import certmanager171 from "../oaspec/certmanager/1.7.json";
+import certmanager1144 from "../oaspec/certmanager/1.14.json";
 
 import flagger1190 from "../oaspec/flagger/1.19.0.json";
 
@@ -39,8 +40,11 @@ export function oaspecFetch(item: string, version: string): K8sDefinitions {
     }
 
     if (item === "certmanager") {
-        if (version === "1.7") {
-            return certmanager171.definitions;
+        switch (version) {
+            case "1.7":
+                return certmanager171.definitions;
+            case "1.14":
+                return certmanager1144.definitions;
         }
     }
 
@@ -90,7 +94,7 @@ export function oaspecFetch(item: string, version: string): K8sDefinitions {
 
 export function availableItemVersions(): { [key: string]: Array<string> } {
     return {
-        "certmanager": ["1.7"],
+        "certmanager": ["1.7", "1.14"],
         "cosign policy-controller": ["0.5.7"],
         "flagger": ["1.19.0"],
         "flux": ["0.27.3", "0.31.2", "2.0.1"],
