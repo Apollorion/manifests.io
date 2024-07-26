3520214f7cbe4c6a8cc24aa9ff3dbd4bb994b56b
apollorion <joey@apollorion.com>
2024-07-26 14:08:12 -0400

    feat: remove workerpoolcontroller

--- lib/oaspec.tsx
+++ lib/oaspec.tsx
@@ -30,8 +30,6 @@ import rancher28 from "../oaspec/rancher/2.8.json";
 
 import spaceliftoperator010 from "../oaspec/spaceliftoperator/0.1.0.json";
 
-import spaceliftworkerpoolcontroller0012 from "../oaspec/spaceliftworkerpoolcontroller/0.0.12.json"
-
 export function oaspecFetch(item: string, version: string): K8sDefinitions {
     if (item === "kubernetes") {
         switch (version) {
@@ -119,12 +117,6 @@ export function oaspecFetch(item: string, version: string): K8sDefinitions {
         }
     }
 
-    if (item === "spacelift workerpool controller") {
-        if (version === "0.0.12") {
-            return spaceliftworkerpoolcontroller0012.definitions;
-        }
-    }
-
     throw new Error("Open Api Spec Not Found");
 }
 
@@ -140,8 +132,7 @@ export function availableItemVersions(): { [key: string]: Array<string> } {
         "opa gatekeeper": ["3.14.0"],
         "prometheus operator": ["0.71.2"],
         "rancher": ["2.8"],
-        "spacelift operator": ["0.1.0"],
-        "spacelift worker pool controller": ["0.0.12"]
+        "spacelift operator": ["0.1.0"]
     }
 }
 
